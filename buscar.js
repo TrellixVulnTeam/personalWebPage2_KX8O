8d8f780e92a969111419a3e3a1470442e4de22ff
sarav22 <56975323+sarav22@users.noreply.github.com>
2021-12-11 15:22:04 +0100

    First try redirecting

--- buscar.js
+++ buscar.js
@@ -12,30 +12,19 @@ $(document).ready(function(){
 
         for(var i=0; i < htmls.length; i++){
             $.get("https://sarav22.github.io/personalWebPage2/"+htmls[i], function(data){
-                
-                var fileDom = $(data);
-                var result= fileDom.val();
-             //  var result2= fileDom.$(":contains("+buscando+")");
-                console.log(result);
-                
                 $(data).ready(function(){
-                    console.log($("*:contains("+buscando+")"));
-                });
-
-                fileDom.find('h2').each(function(){
-                    console.log($(this).text());
-
-                });/*
-                //Se guardan los resultados en un array y las url de dichos resultados
-                for(var j=0; j<result.length; j++){
-                    if(result[j].localName=="p"|| result[j].localName == "h1"|| result[j].localName == "h2"|| result[j].localName == "h3" || result[j].localName=="table"){
-                        searches.push(result[j].innerHTML);
-                        urls.push(htmls[i]);
+                    var result= $("*:contains("+buscando+")");
+                    //Se guardan los resultados en un array y las url de dichos resultados
+                    for(var j=0; j<result.length; j++){
+                        if(result[j].localName=="p"|| result[j].localName == "h1"|| result[j].localName == "h2"|| result[j].localName == "h3" || result[j].localName=="table"){
+                            searches.push(result[j].innerHTML);
+                            urls.push(htmls[i]);
+                        }
                     }
-                }*/
+                });
             });
         }
-  /*      //Se pasan los resultados encontrados a una string separada por ; para poder parsearla posteriormente
+        //Se pasan los resultados encontrados a una string separada por ; para poder parsearla posteriormente
         //ya que la que se genera por defecto es con , y no se parsearía porque el texto contiene comas.
         for(var i=0; i<searches.length; i++){
             if(i==searches.length-1){
@@ -51,6 +40,6 @@ $(document).ready(function(){
         localStorage.setItem("searches", searchesParsed);
         localStorage.setItem("urls", urlsParsed);
         //Se redirige a la página donde se mostrarán los resultados encontrados
-      //  window.location.href = "resultado.html"; */
+        window.location.href = "resultado.html";
     });
 });
