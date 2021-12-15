6028830cb1d0016abac731e0b2a893ecd8cb7cd5
sarav22 <56975323+sarav22@users.noreply.github.com>
2021-12-15 13:36:04 +0100

    Busqueda th td en vez de table

--- buscar.js
+++ buscar.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
         var urls=[];
         var searchesParsed="";
         var urlsParsed="";
+        //Se carga el xml con el contenido de la página
         $.get("https://sarav22.github.io/personalWebPage2/search.xml", function(data){
             var result= $(data).find(":contains("+buscando+")");
             var html="";
@@ -14,12 +15,14 @@ $(document).ready(function(){
                 if($(result[i]).attr("name") != undefined){
                     html=$(result[i]).attr("name");
                 }
-                if(result[i].localName=="p"|| result[i].localName == "h1"|| result[i].localName == "h2"|| result[i].localName == "h3" || result[i].localName=="table"){
+                if(result[i].localName=="p"|| result[i].localName == "h1"|| result[i].localName == "h2"|| result[i].localName == "h3" || result[i].localName=="th"  || result[i].localName=="td"){
                    console.log(result)
                     searches.push(result[i].innerHTML);
                     urls.push(html);
                 }
             } 
+            //Se pasan los resultados encontrados a una string separada por ; para poder parsearla posteriormente
+            //ya que la que se genera por defecto es con , y no se parsearía porque el texto contiene comas.
             for(var i=0; i<searches.length; i++){
                 if(i==searches.length-1){
                     searchesParsed= searchesParsed + searches[i];
@@ -29,19 +32,13 @@ $(document).ready(function(){
                     urlsParsed= urlsParsed + urls[i] + ";";
                 }
             }
+            //Se guardan en localStorage las variables
             localStorage.setItem("busqueda", buscando);
             localStorage.setItem("searches", searchesParsed);
             localStorage.setItem("urls", urlsParsed);
+            //Se redirige a la página donde se mostrarán los resultados encontrados
             window.location.href = "resultado.html";
-           
         });
-        //Se pasan los resultados encontrados a una string separada por ; para poder parsearla posteriormente
-        //ya que la que se genera por defecto es con , y no se parsearía porque el texto contiene comas.
         
-       
-        //Se guardan en localStorage las variables
-        
-        //Se redirige a la página donde se mostrarán los resultados encontrados
-       // window.location.href = "resultado.html";
     });
 });
